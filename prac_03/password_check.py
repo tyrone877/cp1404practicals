4db15f373b3da222325d2c75c4dca03fe3eb1a2e
Tyrone <tyrone.mitchell@my.jcu.edu.au>
2020-08-16 09:42:02 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,8 +1,22 @@
 MINIMUM_PASSWORD_LENGTH = 5
 
-password = input("Enter in your password: ")
-while len(password) < 5:
-    print("Your password must be at least 5 characters long, please re-enter your password")
+
+def main():
+    password = get_password()
+    print_password_length(password)
+
+
+def print_password_length(password):
+    password_length = len(password)
+    print(password_length * "*")
+
+
+def get_password():
     password = input("Enter in your password: ")
-password_length = len(password)
-print(password_length * "*")
+    while len(password) < 5:
+        print("Your password must be at least 5 characters long, please re-enter your password")
+        password = input("Enter in your password: ")
+    return password
+
+
+main()
